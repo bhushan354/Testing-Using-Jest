c6eefa76440386f04e460561641a1e94c4921572
bhushan354 <deshmukhumesh354@gmail.com>
2023-08-24 20:39:31 +0530

    task 1 full

--- sum.js
+++ sum.js
@@ -1,6 +1,15 @@
 function stringLength(string){
+
   const length = string.length;
+  if (length < 1 ){
+     throw new Error( "String must be at least 1 character long" ) ;
+}
+  if (length > 10 ){
+    throw new Error( "String must be less than 10 character" ) ;
+  }
+
   return length;
+
 }
 
 module.exports = stringLength;
--- sum.test.js
+++ sum.test.js
@@ -1,7 +1,17 @@
 const stringLength = require('./sum.js');
-
+const inputString = "Hello";
 test('stringLength test', () => {
-  const inputString = "Hello, world!";
+ 
   const result = stringLength(inputString);
   expect(result).toBe(inputString.length);
+});
+
+test('throws error for less than 1 character', () => {
+  const inputString = '';
+  expect(() => stringLength(inputString)).toThrowError("String must be at least 1 character long");
+});
+
+test('throws error for string longer than 10 characters', () => {
+  const inputString = 'ThisIsTooLong';
+  expect(() => stringLength(inputString)).toThrowError("String must be less than 10 character");
 });
